d31337c0538bc11f45130a9a3294c290609e858f
abodhare <abodhare1234+1@gmail.com>
2020-05-01 00:58:29 +0530

    two nodes in the hashtable

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
 module Lib
     ( app
     ) where
@@ -22,42 +21,59 @@ app = do
 
 app_ :: [String] -> IO ()
 app_ args = do
-    a <- parseAddr args
-    hashtable a
+    let a = parseAddr args "--a"
+    let b = parseAddr args "--b"
+    hashtable a b
 
-parseAddr :: [String] -> IO (C.HostName, C.ServiceName)
-parseAddr ("--addr" : ipA : pA : _) = return (ipA, pA)
-parseAddr (_:xs) = parseAddr xs
-parseAddr [] = return ("","")
+parseAddr :: [String] -> String -> (C.HostName, C.ServiceName)
+parseAddr (x : ipA : pA : xs) s
+    | x == s = (ipA, pA)
+    | otherwise = parseAddr (ipA : pA : xs) s
+parseAddr (_:xs) s = parseAddr xs s
+parseAddr [] _ = ("","")
 
-hashtable :: (C.HostName, C.ServiceName) -> IO ()
-hashtable a@(ah, ap) = do
+hashtable :: (C.HostName, C.ServiceName) -> (C.HostName, C.ServiceName) -> IO ()
+hashtable a b = do
     ht <- HT.new :: IO (HashTable B.ByteString B.ByteString)
-    listenHT ht a
+    listenHT ht a b
 
-lookupHT :: HashTable B.ByteString B.ByteString -> B.ByteString -> IO B.ByteString
-lookupHT ht x = HT.lookup ht x >>= (\case Nothing -> return "not available\n"
-                                          Just s -> return (B.concat ["data: ", s, "\n"]))
+lookupString :: Maybe B.ByteString -> B.ByteString
+lookupString Nothing = "not available\n"
+lookupString (Just s) = B.concat ["data: ", s, "\n"]
 
-insertHT :: HashTable B.ByteString B.ByteString -> B.ByteString -> B.ByteString -> IO B.ByteString
-insertHT ht k v = HT.insert ht k v >> return (B.concat ["data stored: ", k, ", ", v, "\n"])
+insertString :: B.ByteString -> B.ByteString -> B.ByteString
+insertString k v = B.concat ["data stored: ", k, ", ", v, "\n"]
 
-listenHT :: HashTable B.ByteString B.ByteString -> (C.HostName, C.ServiceName) -> IO ()
-listenHT ht (ah, as) = C.serve (C.Host ah) as $ \(connSoc, remoteAddr) -> do
+listenHT :: HashTable B.ByteString B.ByteString -> (C.HostName, C.ServiceName) -> (C.HostName, C.ServiceName) -> IO ()
+listenHT ht (ah, as) b = C.serve (C.Host ah) as $ \(connSoc, remoteAddr) -> do
     B.putStr "TCP connection established from " >> print remoteAddr
     SO.setSocketOption connSoc SO.KeepAlive 10000
-    runLoop ht connSoc
+    runLoop ht b connSoc
 
-runLoop :: HashTable B.ByteString B.ByteString -> C.Socket -> IO ()
-runLoop ht connSoc = do
+runLoop :: HashTable B.ByteString B.ByteString -> (C.HostName, C.ServiceName) -> C.Socket -> IO ()
+runLoop ht b connSoc = do
     s <- C.recv connSoc 128
     case s of
         Nothing -> C.send connSoc "not available"
         Just val -> case B.words val of
-                        ["get", x] -> lookupHT ht x >>= C.send connSoc >> runLoop ht connSoc
-                        ["put", x, y] -> insertHT ht x y >>= C.send connSoc >> runLoop ht connSoc
+                        ["get", x] -> HT.lookup ht x >>= lookupOther b x >>= C.send connSoc >> runLoop ht b connSoc
+                        ["getID", x] -> (lookupString <$> HT.lookup ht x) >>= C.send connSoc >> runLoop ht b connSoc
+                        ["put", x, y] -> HT.insert ht x y >> C.send connSoc (insertString x y) >> runLoop ht b connSoc
                         ("q":_) -> C.send connSoc "shutting the server down, bye bye"
-                        _          -> C.send connSoc helpString >> runLoop ht connSoc
+                        _          -> C.send connSoc helpString >> runLoop ht b connSoc
+
+lookupOther :: (C.HostName, C.ServiceName) -> B.ByteString -> Maybe B.ByteString -> IO B.ByteString
+lookupOther b x Nothing = lookupOther_ b x
+lookupOther _ _ s@(Just x) = return $ lookupString s
+
+lookupOther_ :: (C.HostName, C.ServiceName) -> B.ByteString -> IO B.ByteString
+lookupOther_ (bh, bs) x = C.connect bh bs $ \(connSoc, remoteAddr) -> do
+                            B.putStr "Connection established to " >> print remoteAddr
+                            C.send connSoc (B.unwords ["getID", x])
+                            s <- C.recv connSoc 128
+                            case s of
+                                Nothing -> return "not available"
+                                Just val -> return val
 
 helpString :: B.ByteString
 helpString = "The syntax is:\n get key - To get a value\n put key value - To store values\n q - To quit"
